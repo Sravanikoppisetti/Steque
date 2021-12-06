a2106375c6414802db1aa7117b850913d2a22cc9
sravanikoppisetti <koppisettisravs@msitprogram.com>
2021-12-06 11:19:25 +0530

    pop

--- Steque.java
+++ Steque.java
@@ -91,10 +91,16 @@ public class Steque<Item> implements Iterable<Item>
      * pops a least recent item in steque.
      * @return Item object from steque.
      */
+    //Time Complexity:O(1), Space Complexity:O(N)
     public Item pop() {
-
+        if(isEmpty())
+            throw new NoSuchElementException();
+        Item item=first.item;
+        first=first.next;
+        n--;
+        return item;
     }
-    
+
     /**
      * checks to see if steque is empty.
      * @return true if steque is empty, false otherwise.
