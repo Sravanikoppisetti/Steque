48578e84422e1ae582f224c0c1a4da23d458762d
sravanikoppisetti <koppisettisravs@msitprogram.com>
2021-12-06 11:50:04 +0530

    Array_isEmpty

--- Steque.java
+++ Steque.java
@@ -101,9 +101,11 @@ public class Steque<Item> implements Iterable<Item> {
      * checks to see if steque is empty.
      * @return true if steque is empty, false otherwise.
      */
+    //Time complexity:O(1) Space Complexity:0
     public boolean isEmpty() {
-
+        return n==0;
     }
+
     
     /**
      * return the number of elements currently in the steque.
